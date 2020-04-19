b3bef860663c803668d644767692f488056d38ce
Antoine Trapet <ayodio@gmail.com>
2020-04-19 17:53:10 +0200

    ajout du css du theme enfant après le parent

--- wp-content/themes/twentyeleven-10ansvefa/functions.php
+++ wp-content/themes/twentyeleven-10ansvefa/functions.php
@@ -1,6 +1,14 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles() {
-  wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+function my_theme_enqueue_styles() {
+  // You'll find this somewhere in the parent theme in a wp_enqueue_style('parent-style').
+  $parent_style = 'parent-style';
+
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    array( $parent_style ),
+    wp_get_theme()->get('Version')
+  );
 }
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
